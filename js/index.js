13c9ef48e1ee781fdea05288f610bf1f4eaeac44
Manish Tomar <Manish.tomar@incedoinc.com>
2017-12-22 15:45:46 +0530

    First commit

---
+++ js/index.js
@@ -0,0 +1,170 @@
+$(document).ready(function(){
+    
+	siteUrl = "";
+	dbData = [];
+	sourceData = [];
+	verticalDashDiv = '<div class="vertical-dot-line"></div>';
+	horizontalDashDiv = '<div class="horizontal-dot-line"></div>';
+	dashDiv = '<div>' + verticalDashDiv + '' + horizontalDashDiv + '</div>'; 
+	
+	// manage height of left and right panel
+	var mainHeight=$('#main').height(), navHeight = $('#main nav').height(), leftHeight = $('#left').height(),
+	total = $(document).height() - mainHeight + leftHeight;
+	$("#left, #right").height(total);
+	
+	// On page load change fa icon css
+	faIconWidth = $('#repositories i').width();
+	$('.vertical-dot-line').css('margin-left',faIconWidth/2).css('margin-top','-3px');
+	$('.horizontal-dot-line').css('margin-left',faIconWidth/2);
+	$('.horizontal-dot-line').next().css('margin-left', $('.horizontal-dot-line').width()+8).css('margin-top',-1*faIconWidth);
+	
+	
+	$("#topNav li").click(function(){
+		var $this = $(this);
+		$('#topNav li').removeClass('active');
+		$this.addClass('active');
+	});
+	
+	$("#modalSubmit").click(function(){
+		/*ajaxParam = {};
+		ajaxParam["url"] = "https://www.w3schools.com/bootstrap4/checkDBConnectionStatus";
+		ajaxParam["method"] = "POST";
+		ajaxParam["dbType"] = $('#dbType').val();
+		ajaxParam["dbUrl"] = $('#dbUrl').val();
+		ajaxParam["successCallback"] = "dbSuccessCallback";
+		ajaxParam["completeCallback"] = "dbCompleteCallback";
+		ajaxParam["errorCallback"] = "dbErrorCallback";
+		makeAjaxCall(ajaxParam);*/
+		//$('#myModal').modal('hide');
+		sourceData = [{"tableName":"Table 0","status":true},{"tableName":"Table 1","status":true},{"tableName":"Table 2","status":true},{"tableName":"Table 3","status":true},{"tableName":"Table 4","status":true},{"tableName":"Table 5","status":true},{"tableName":"Table 6","status":true},{"tableName":"Table 7","status":true},{"tableName":"Table 8","status":true},{"tableName":"Table 9","status":true}];
+		sourceDataSuccess(sourceData);
+	});
+	
+	$("#newDB").click(function(){
+		$('#myModal').modal('show');
+		/*ajaxParam = {};
+		ajaxParam["url"] = "https://www.w3schools.com/bootstrap4/checkDBConnectionStatus";
+		ajaxParam["method"] = "POST";
+		ajaxParam["dbType"] = $('#dbType').val();
+		ajaxParam["dbUrl"] = $('#dbUrl').val();
+		ajaxParam["successCallback"] = "dbSuccessCallback";
+		ajaxParam["completeCallback"] = "dbCompleteCallback";
+		ajaxParam["errorCallback"] = "dbErrorCallback";
+		makeAjaxCall(ajaxParam);*/
+		dbData = [{"passRequired":false,"dbType":"Hbase","status":true,"userNameRequired":false,"urlRequired":true},{"passRequired":true,"dbType":"MySql","status":true,"userNameRequired":true,"urlRequired":true}];
+		dbSuccessCallback(dbData);
+		$('#myModal').modal('show');
+		
+	});
+	
+	$('#myModal').on('hide.bs.modal', function (e) {
+		$('#modalContent').html('');
+		$('#moreContent').html('');
+		$('.modal-content').removeAttr('style');
+	});
+	
+	$(document).on("change", "#dbType" , function() {	
+		var $this = $(this);
+		$('#modalContent input:text').closest('.modal-section').remove();
+		$.each(dbData, function(iO,vO){
+				if($this.val() == vO.dbType)
+				{
+					$.each(vO, function( index, value ) {
+						if((index == 'passRequired' || index == 'userNameRequired' || index == 'urlRequired') && value)
+						{	
+							var label = index, newLabel=label[0].toUpperCase();
+							label = label.replace('Required',''); 
+							for(var i=1; i<label.length; i++){     
+								if(label[i] == label[i].toUpperCase())
+									newLabel += ' '+label[i];
+								else
+									newLabel += label[i];	
+							}
+							$('#modalContent').append('<div class="modal-section"><label>'+newLabel+':</label> <input type="text" id="'+value+'" name="'+value+'"/></div>');		
+						}
+					});	
+				}	
+		});
+	});
+	
+	$(document).on("click", ".modal-text a", function(){
+		var $this = $(this);
+		if($this.hasClass('table-select-not'))
+		{
+			$this.removeClass('table-select-not');
+			$this.addClass('table-select');
+			$this.css('color', 'blue');
+		} else if($this.hasClass('table-select'))
+		{
+			$this.removeClass('table-select');
+			$this.addClass('table-select-not');
+			$this.css('color', 'black');
+		}	
+	});
+	
+	$(document).on("click", "#selectTable", function(){
+		var $this = $(this);
+		
+		$('#myModal').modal('show');
+		//var leftMargin = $('.vertical-dot-line').parent().find('i:first').width()/2;
+		//$('#moreContent').append('<div styple="margin-left: '+ ($('#dbTable').width() + 70) +'"><button type="button" class="btn btn-primary" id="modalSubmit">Submit</button></div>');
+	});
+	
+	$('#repositories').click(function(){
+		var $this = $(this);
+	});
+});
+
+function makeAjaxCall(ajaxParam) {
+	
+	params = {};
+	for (var key in ajaxParam) {
+		if (key != 'successCallback' && key != 'errorCallback' && key != 'completeCallback' && key != 'url' && key != 'method') {
+			params[key] = ajaxParam[key];
+		}
+	}
+	params = JSON.stringify(params);
+    $.ajax({
+        success : ajaxParam["successCallback"],
+        error : ajaxParam['errorCallback'],
+		complete : ajaxParam['dbCompleteCallback'],
+        data : params,
+        url : ajaxParam['url'],
+        type : ajaxParam['method'],
+        dataType : 'json',
+		crossDomain: true,
+    });
+}
+function dbSuccessCallback(data){
+		$('#modalContent').html('');
+		$('#moreContent').html('');
+		if(data && data.length>0)
+		{	
+			var length = data.length;
+			$('#modalContent').append('<div class="modal-section"><label>Database Type:</label><select id="dbType"></select></div>');
+			for(var i=0; i<length; i++)
+			{	
+				$('#dbType').append('<option>'+data[i].dbType+'</option>');
+			}
+			$.each(data[0], function( index, value ) {
+			if((index == 'passRequired' || index == 'userNameRequired' || index == 'urlRequired') && value)
+					$('#modalContent').append('<div class="modal-section"><label>Url:</label> <input type="text" id="'+value+'" name="'+value+'"/></div>');
+			});
+		}	
+}
+function dbCompleteCallback(){
+	
+	$('#modalClose').click();
+}
+function dbErrorCallback(){
+	$('#modalClose').click();
+}
+function sourceDataSuccess(data){
+	$('#moreContent').html('<div id="dbTable" style="display:none;"><div class="modal-text"></div></div>');
+	$('.modal-content').height($('.modal-content').height()+$('#dbTable').height());
+	$.each(sourceData,function(i,v){
+		$('.modal-text').append('<a href="javascript:void(0)" style="color: black;" class="table-select-not">'+v.tableName+'</a><br/>');
+	});
+	$('#moreContent').append('<div><button type="button" class="btn btn-primary" id="selectTable" style="margin-left: '+ ($('#dbTable').width() + 50) +'px;margin-top: 100px;">Select</button></div>');
+	$('#dbTable').show();
+}
